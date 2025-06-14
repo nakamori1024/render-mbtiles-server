e68407fe0da143d2f57d0cb09b4a9e0f2dd8ddc3
nakamori1024 <79179090+nakamori1024@users.noreply.github.com>
2025-06-15 00:38:18 +0900

    Add comment

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ from fastapi import HTTPException
 
 
 def sanitize_filename(file_name: str) -> str:
+    """
+    Sanitize the filename to prevent directory traversal attacks.
+    """
     basename = os.path.basename(file_name)
     if basename != file_name:
         raise HTTPException(status_code=400, detail="Invalid characters in filename")
